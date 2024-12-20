1c0c71c219c0158debefb3f513308db59f1d2cd7
Phillweston <2436559745@qq.com>
2024-12-21 01:31:18 +0800

    Add vhdl example code

---
+++ vhdl/rtl/my_pakg.vhd
@@ -0,0 +1,7 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+package my_pakg is
+    type mem is array (7 downto 0) of std_logic_vector(255 downto 0); -- 定义数组
+    type STATE_CODE is (S0, S1, S2, S3);
+end my_pakg;
